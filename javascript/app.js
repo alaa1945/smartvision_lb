935698f922d43be5ac61cca294ff64ededbf31d2
Alaa A.E <80849327+alaa1945@users.noreply.github.com>
2023-02-01 17:59:54 +0200

    first

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,6 @@ const mobile_menu = document.querySelector(".mobile-menu");
 const mobile_navlist = document.querySelector(".nav-list");
 const mobile_navlinks = document.querySelectorAll(".nav-links");
 
-
 // show nav on btn click
 mobile_menu.addEventListener("click", function () {
   mobile_navlist.classList.toggle("active");
@@ -17,11 +16,18 @@ mobile_navlinks.forEach((link) => {
   });
 });
 document.addEventListener("scroll", () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 250) {
-      header.style.backgroundColor = "#ffffff";
-      header.style.paddingBottom = "20px";
-    } else {
-      header.style.backgroundColor = "transparent";
-    }
-  });
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#ffffff";
+    header.style.paddingBottom = "20px";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+function closeIcon() {
+  if (mobile_navlist.classList.contains("active")) {
+    mobile_menu.src = "images/close-icon.svg";
+  } else {
+    mobile_menu.src = "images/mobile-menu.svg";
+  }
+}
